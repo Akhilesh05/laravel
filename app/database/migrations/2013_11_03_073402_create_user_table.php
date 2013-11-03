fd49d067928b0073bb62f2b83231e4433c3d8a6a
akhgop <akhgop@vm-0.akhgop.koding.kd.io>
2013-11-03 07:39:20 +0000

    db

--- app/database/migrations/2013_11_03_073402_create_user_table.php
+++ app/database/migrations/2013_11_03_073402_create_user_table.php
@@ -15,6 +15,9 @@ class CreateUserTable extends Migration {
 		Schema::create('users', function(Blueprint $table)
 		{
 			$table->increments('id');
+			$table->string('email');
+			$table->string('password');
+			$table->string('fullName');
 			$table->timestamps();
 		});
 	}
