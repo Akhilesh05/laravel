90cf43174ee1b89992f24d3c12fb644009bbdabb
Girish Gopaul <akhile@dr.com>
2013-11-03 09:47:25 +0000

    Update form.php

--- app/views/form.php
+++ app/views/form.php
@@ -1,9 +1,9 @@
 <form action="/form" method="post">
   <label for="name">name: </label>
   <input type="text" name="name" />
-  <label for="name">email: </label>
+  <label for="email">email: </label>
   <input type="email" name="email" />
-  <label for="name">password: </label>
+  <label for="password">password: </label>
   <input type="password" name="password" />
   <input type="submit" value="post" />
 </form>
