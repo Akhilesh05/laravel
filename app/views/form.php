fca684ad5e29431392281dcf462f4e28dda46925
Girish Gopaul <akhile@dr.com>
2013-11-03 09:44:08 +0000

    Update form.php

--- app/views/form.php
+++ app/views/form.php
@@ -1,5 +1,7 @@
 <form action="/form" method="post">
   <label for="name">name: </label>
   <input type="text" name="name" />
+  <label for="name">name: </label>
+  <label for="name">name: </label>
   <input type="submit" value="post" />
 </form>
